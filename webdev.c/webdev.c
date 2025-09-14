d504663715e456d2a0452101da1d91ed885ba450
Filipe Paschoal Góes <filipe@example.com>
2025-09-14 18:26:52 -0300

    End of 1st video

--- webdev.c/webdev.c
+++ webdev.c/webdev.c
@@ -1,6 +1,7 @@
 #include "webdev.h"
 #include "lib/birchutils.h"
 
+bool continuation;
 
 void syslogger(int16 priority, int8 *str){
     openlog("webdev.c", 0, LOG_SYSLOG);
@@ -22,10 +23,37 @@ int32 setupserver(int8 *host, int16 port){
 
     n= $6 bind(s, (struct sockaddr *)&sock, sizeof(sock));
     assert(!n);
+
     n =$6 listen(s, 50);
     assert(!n);
 
     return s;
+}
+
+int8 mainloop(int32 s){
+    struct sockaddr_in sock;
+    int8 *str, *host;
+    int8 buf[128];
+    int16 port;
+    int32 c;
+    unsigned int size;
+
+    size=0;
+    c = $6 accept ($i s, (struct sockaddr *)&sock, (unsigned int *)&size);
+    if (c>0); else {
+        str = $8 "accept() failed";
+        logwarning(str);
+        sleep(1);
+        return 1;
+    }
+    if (fork()){
+        host = todotted(sock.sin_addr.s_addr);
+        port = nstoh($6 spcek.sin_port);
+        zero(buf);
+
+        snprintf($c buf, 127, "Client connected from %s:%d", $c host, $i port);
+        log(buf);
+    }
 }
 
 void usage (int8* arg){
